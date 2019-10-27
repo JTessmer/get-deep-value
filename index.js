3ba7de7620f8ae453917e79917e146c896721bbf
Jesse Tessmer <jesse.tessmer@gmail.com>
2019-10-26 21:13:49 -0500

    Handle cases where a midpoint segment is a non-object

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ module.exports = function getDeepValue(base, path, fallback = null) {
 		.filter(segment => segment.length)
 
 	for (let i = 0; i < segments.length; i++) {
-		if (typeof curVarScope[segments[i]] === 'undefined') return fallback
-		if (i + 1 === segments.length) return curVarScope[segments[i]]
+		const nextVarScope = curVarScope[segments[i]]
 
-		curVarScope = curVarScope[segments[i]]
+		if (i + 1 === segments.length) return nextVarScope
+		if (typeof nextVarScope !== 'object') return fallback
+
+		curVarScope = nextVarScope
 	}
 }
